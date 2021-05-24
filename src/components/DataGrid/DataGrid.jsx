bf6c774b63bf6a097b0c853b2180b9ecf66ca4dc
Wagner <wagnerburgie@gmail.com>
2021-05-24 12:08:39 -0300

    Componenet DataGrid passar columns por props

--- src/components/DataGrid/DataGrid.jsx
+++ src/components/DataGrid/DataGrid.jsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
-//{id: "5791", codigo: "A971-1", nome: "ALIMENTADORA J. P. ALVES/ESCOLAR/IDA"}
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  { field: 'codigo', headerName: 'Codigo', width: 150 },
-  { field: 'nome', headerName: 'Nome', width: 150 },
-];
 
-export default function DataGridDemo({ data }) {
+export default function DataGridDemo({ data, columns }) {
   let rows = data;
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={10} checkboxSelection />
+    <div style={{ height: '100vh', width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} pageSize={10} />
     </div>
   );
 }
