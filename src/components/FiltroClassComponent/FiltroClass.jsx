b23cd50fe70359ae41461431921b1e28951864d8
Wagner <wagnerburgie@gmail.com>
2021-05-24 12:09:23 -0300

    Passando as colunas para o DataGrid

--- src/components/FiltroClassComponent/FiltroClass.jsx
+++ src/components/FiltroClassComponent/FiltroClass.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Component } from "react";
 import DataGrid from "../DataGrid/DataGrid"
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'codigo', headerName: 'Codigo', width: 150 },
+    { field: 'nome', headerName: 'Nome', width: 150 },
+  ];
 class FiltroClass extends Component {
     constructor() {
         super();
@@ -19,7 +25,7 @@ class FiltroClass extends Component {
         this.setState({ ...this.state, linhas })
     }
     render() {
-        return (<div><DataGrid data={this.state.linhas} /></div>);
+        return (<div><DataGrid data={this.state.linhas} columns={columns} /></div>);
     }
 }
 export default FiltroClass;
