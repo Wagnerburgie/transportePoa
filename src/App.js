3e581ab76e342e7d1b3246bfe43baa738c940e6d
Wagner <wagnerburgie@gmail.com>
2021-05-24 12:09:02 -0300

    Alterção Grid material Ui app

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import GoogleMap from "./components/GoogleMap/GoogleMap"
 import 'fontsource-roboto';
 import Linhas from './dados/linhas'
 import FiltroClassComponent from "./components/FiltroClassComponent/index"
+import { Container, Typography, Grid } from "@material-ui/core"
 
-import { Container, Typography } from "@material-ui/core"
+
 class App extends Component {
   constructor() {
     super();
     this.linhas = new Linhas();
-    //this.linhas.notificar();
   }
   render() {
     return (
       <Fragment>
-        <Container component="article" maxWidth="lg">
-          <Typography variant="h3" component="h1" align="left" >Filtro</Typography>
-          <FiltroClassComponent adicionarLinha={this.linhas.adicionarLinha.bind(this.linha)} linhas={this.linhas} />
-        </Container>
-        <Container component="map" maxWidth="lg" align="right">
-          <GoogleMap />
-        </Container>
+        <Grid container>
+          <Grid item xs={3}>
+            <Typography variant="h3" component="h1" align="left" >Filtro</Typography>
+            <FiltroClassComponent adicionarLinha={this.linhas.adicionarLinha.bind(this.linha)} linhas={this.linhas} />
+          </Grid>
+          <Grid item xs={9}>
+            <GoogleMap />
+          </Grid>
+        </Grid>
       </Fragment>
     );
   }
