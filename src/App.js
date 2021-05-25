7e8a894b4750b896909d34bbbf342bb0af3ea4dd
Wagner <wagnerburgie@gmail.com>
2021-05-25 13:44:58 -0300

    Metodo para pegar Markers e montar linha de Onibus

--- src/App.js
+++ src/App.js
@@ -4,35 +4,49 @@ import "./App.css";
 import GoogleMap from "./components/GoogleMap/GoogleMap"
 import 'fontsource-roboto';
 import Linhas from './dados/linhas'
+import Markers from './dados/markers'
 import FiltroClassComponent from "./components/FiltroClassComponent/index"
 import { Container, Typography, Grid } from "@material-ui/core"
+
+const gridStyle = {
+  height: '100%'
+};
+
+const center = {
+  lat: -30.0277, lng: -51.2287
+};
 
 
 class App extends Component {
   constructor() {
     super();
     this.linhas = new Linhas();
+    this.markers = new Markers();
+    this.getMarkers = this.getMarkers.bind(this)
   }
+
+  getMarkers(id) {
+    this.markers.getMarkers(id);
+  }
+
   render() {
     return (
-      <Fragment>
-        <Grid container>
-          <Grid item xs={3}>
-            <Typography variant="h3" component="h1" align="left" >Filtro</Typography>
-            <FiltroClassComponent adicionarLinha={this.linhas.adicionarLinha.bind(this.linha)} linhas={this.linhas} />
-          </Grid>
-          <Grid item xs={9}>
-            <GoogleMap />
-          </Grid>
+      <Grid container style={gridStyle}>
+        <Grid item xs={3}>
+          <FiltroClassComponent
+            adicionarLinha={this.linhas.adicionarLinha.bind(this.linha)}
+            linhas={this.linhas}
+            markers={this.markers}
+            selecionarLinha={(id) => { this.getMarkers(id) }}
+          />
         </Grid>
-      </Fragment>
+        <Grid item xs={9}>
+          <GoogleMap markers={this.markers} center={center} />
+        </Grid>
+      </Grid>
     );
   }
 }
 
-
-function aoEnviarForm(dados) {
-  console.log(dados);
-}
 
 export default App;
