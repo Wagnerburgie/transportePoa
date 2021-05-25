53c95ba94c08114ca2af303527d0d4215705c1c1
Wagner <wagnerburgie@gmail.com>
2021-05-25 18:20:25 -0300

    Alteração retirada de componentes não usados

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./App.css";
 //import Filtro from "./components/Filtro/Filtro";
-import GoogleMap from "./components/GoogleMap/GoogleMap"
+import GoogleMap from "./components/GoogleMap/GoogleMap";
 import 'fontsource-roboto';
-import Linhas from './dados/linhas'
-import Markers from './dados/markers'
-import FiltroClassComponent from "./components/FiltroClassComponent/index"
-import { Container, Typography, Grid } from "@material-ui/core"
+import Linhas from './dados/linhas';
+import Markers from './dados/markers';
+import FiltroClassComponent from "./components/FiltroClassComponent/index";
+import { Grid } from "@material-ui/core";
 
 const gridStyle = {
   height: '100%'
@@ -22,7 +22,7 @@ class App extends Component {
     super();
     this.linhas = new Linhas();
     this.markers = new Markers();
-    this.getMarkers = this.getMarkers.bind(this)
+    this.getMarkers = this.getMarkers.bind(this);
   }
 
   getMarkers(id) {
