af628cc6f2f182740b81663edce0f0b5b55851de
Wagner <wagnerburgie@gmail.com>
2021-05-25 18:20:41 -0300

    Alteração retirada do footerCounter

--- src/components/DataGrid/DataGrid.jsx
+++ src/components/DataGrid/DataGrid.jsx
@@ -3,8 +3,6 @@ import { DataGrid } from '@material-ui/data-grid';
 
 export default function DataGridDemo({ rows, columns, onCellClick }) {
   return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} onCellClick={onCellClick} />
-    </div>
+      <DataGrid rows={rows} columns={columns} onCellClick={onCellClick} hideFooterSelectedRowCount />
   );
 }
