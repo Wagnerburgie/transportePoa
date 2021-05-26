849f6384cee6c899856dce40664df3825ddb4fd7
Wagner <wagnerburgie@gmail.com>
2021-05-26 12:24:46 -0300

    Alteração

--- src/components/DataGrid/DataGrid.jsx
+++ src/components/DataGrid/DataGrid.jsx
@@ -3,6 +3,6 @@ import { DataGrid } from '@material-ui/data-grid';
 
 export default function DataGridDemo({ rows, columns, onCellClick }) {
   return (
-      <DataGrid rows={rows} columns={columns} onCellClick={onCellClick} hideFooterSelectedRowCount />
+    <DataGrid rows={rows} columns={columns} onCellClick={onCellClick} hideFooterSelectedRowCount />
   );
 }
